0f1a535a524be37b606e4fe5a507c425070e6c2c
oip <oip@anadeainc.com>
2023-01-08 14:41:57 +0200

    verify

--- src/template/templates_mail.js
+++ src/template/templates_mail.js
@@ -1,7 +1,187 @@
-const verifyTemplate = (email, verificationToken) => ({
+const verifyTemplate = (userName, email, verificationCode) => ({
   to: email,
   subject: 'Verification email',
-  html: `<a target="_blank" href="http://localhost:3000/api/auth/verify/${verificationToken}">Verify your account</a>`
+  html: `<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+      <meta http-equiv="content-type" content="text/html; charset=utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0;">
+      <meta name="format-detection" content="telephone=no"/>
+    <style>
+      body { margin: 0; padding: 0; min-width: 100%; width: 100% !important; height: 100% !important;}
+      body, table, td, div, p, a {
+        -webkit-font-smoothing: antialiased;
+        text-size-adjust: 100%;
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%;
+        line-height: 100%;
+      }
+      table, td { mso-table-lspace: 0pt;
+        mso-table-rspace: 0pt;
+        border-collapse: collapse !important;
+        border-spacing: 0;
+      }
+      img { border: 0;
+        line-height: 100%;
+        outline: none;
+        text-decoration: none;
+        -ms-interpolation-mode: bicubic;
+      }
+      #outlook a { padding: 0; }
+      .ReadMsgBody { width: 100%; } .ExternalClass { width: 100%; }
+      .ExternalClass,
+      .ExternalClass p,
+      .ExternalClass span,
+      .ExternalClass font,
+      .ExternalClass td,
+      .ExternalClass div { line-height: 100%; }
+      
+      @media all and (min-width: 560px) {
+      .container { border-radius: 8px; -webkit-border-radius: 8px; -moz-border-radius: 8px; -khtml-border-radius: 8px;}
+      }
+      
+      a, a:hover {
+        color: #127DB3;
+      }
+      .footer a, .footer a:hover {
+        color: #999999;
+      }
+     </style>
+    <title>Confirm email</title>
+    </head>
+
+<body topmargin="0" rightmargin="0" bottommargin="0" leftmargin="0" marginwidth="0" marginheight="0" width="100%"
+  style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%; height: 100%; -webkit-font-smoothing: antialiased; text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; line-height: 100%;
+    background-color: #F0F0F0;
+    color: #000000;"
+  bgcolor="#F0F0F0"
+  text="#000000">
+
+<table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%;" class="background"><tr><td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0;"
+bgcolor="#F0F0F0">
+
+<!-- WRAPPER -->
+<table border="0" cellpadding="0" cellspacing="0" align="center"
+width="560" style="border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
+max-width: 560px;" class="wrapper">
+
+<tr>
+<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+padding-top: 20px; padding-bottom: 20px;">
+
+<!-- PREHEADER -->
+<!-- Set text color to background color -->
+<div style="display: none; visibility: hidden; overflow: hidden; opacity: 0; font-size: 1px; line-height: 1px; height: 0; max-height: 0; max-width: 0;
+color: #F0F0F0;" class="preheader">
+Available on&nbsp;GitHub and&nbsp;CodePen. Highly compatible. Designer friendly. More than 50%&nbsp;of&nbsp;total email opens occurred on&nbsp;a&nbsp;mobile device&nbsp;— a&nbsp;mobile-friendly design is&nbsp;a&nbsp;must for&nbsp;email campaigns.</div>
+
+<!-- LOGO -->
+<!-- Image text color should be opposite to background color. Set your url, image src, alt and title. Alt text should fit the image size. Real image size should be x2. URL format: http://domain.com/?utm_source={{Campaign-Source}}&utm_medium=email&utm_content=logo&utm_campaign={{Campaign-Name}} -->
+
+</td>
+</tr>
+
+<!-- End of WRAPPER -->
+</table>
+
+<!-- WRAPPER / CONTEINER -->
+<!-- Set conteiner background color -->
+<table border="0" cellpadding="0" cellspacing="0" align="center"
+bgcolor="#FFFFFF"
+width="560" style="border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
+max-width: 560px;" class="container">
+
+<!-- HEADER -->
+<!-- Set text color and font family ("sans-serif" or "Georgia, serif") -->
+<tr>
+<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 24px; font-weight: bold; line-height: 130%;
+padding-top: 25px; color: #000000; font-family: sans-serif;" class="header">
+${userName}, confirm your email address!
+</td>
+</tr>
+
+<!-- SUBHEADER -->
+<!-- Set text color and font family ("sans-serif" or "Georgia, serif") -->
+<!-- <tr>
+<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-bottom: 3px; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 18px; font-weight: 300; line-height: 150%;
+padding-top: 5px;
+color: #000000;
+font-family: sans-serif;" class="subheader">
+Available on&nbsp;GitHub and&nbsp;CodePen
+</td>
+</tr> -->
+
+<!-- HERO IMAGE -->
+<!-- Image text color should be opposite to background color. Set your url, image src, alt and title. Alt text should fit the image size. Real image size should be x2 (wrapper x2). Do not set height for flexible images (including "auto"). URL format: http://domain.com/?utm_source={{Campaign-Source}}&utm_medium=email&utm_content={{Ìmage-Name}}&utm_campaign={{Campaign-Name}} -->
+
+<!-- PARAGRAPH -->
+<!-- Set text color and font family ("sans-serif" or "Georgia, serif"). Duplicate all text styles in links, including line-height -->
+<tr>
+<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%;
+padding-top: 25px; color: #000000; font-family: sans-serif;" class="paragraph">
+More than 50%&nbsp;of&nbsp;total email opens occurred on&nbsp;a&nbsp;mobile device&nbsp;— a&nbsp;mobile-friendly design is&nbsp;a&nbsp;must for&nbsp;email campaigns.
+</td>
+</tr>
+
+<!-- BUTTON -->
+<!-- Set button background color at TD, link/text color at A and TD, font family ("sans-serif" or "Georgia, serif") at TD. For verification codes add "letter-spacing: 5px;". Link format: http://domain.com/?utm_source={{Campaign-Source}}&utm_medium=email&utm_content={{Button-Name}}&utm_campaign={{Campaign-Name}} -->
+<tr>
+<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+padding-top: 25px; padding-bottom: 5px;" class="button">
+<table border="0" cellpadding="0" cellspacing="0" align="center" style="max-width: 240px; min-width: 120px; border-collapse: collapse; border-spacing: 0; padding: 0;"><tr><td align="center" valign="middle" style="padding: 12px 24px; margin: 0; text-decoration: underline; border-collapse: collapse; border-spacing: 0; border-radius: 4px; -webkit-border-radius: 4px; -moz-border-radius: 4px; -khtml-border-radius: 4px;"
+bgcolor="#E9703E"><a target="_blank" style="text-decoration: underline; color: #FFFFFF; font-family: sans-serif; font-size: 17px; font-weight: 400; line-height: 120%;" href="http://localhost:3001/verify/${verificationCode}">
+Validate my email
+</a>
+</td></tr></table>
+</td>
+</tr>
+
+<!-- LIST -->
+<tr>
+<!-- LIST ITEM -->
+
+<!-- LIST ITEM IMAGE -->
+<!-- Image text color should be opposite to background color. Set your url, image src, alt and title. Alt text should fit the image size. Real image size should be x2 -->
+<!-- LIST ITEM TEXT -->
+<!-- Set text color and font family ("sans-serif" or "Georgia, serif"). Duplicate all text styles in links, including line-height -->
+</tr>
+
+<tr>
+<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+padding-top: 25px;" class="line"><hr
+color="#E0E0E0" align="center" width="100%" size="1" noshade style="margin: 0; padding: 0;" />
+</td>
+</tr>
+<tr>
+<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%;
+padding-top: 20px; padding-bottom: 25px; color: #000000; font-family: sans-serif;" class="paragraph">
+Have a&nbsp;question? <a href="mailto:support@test.com" target="_blank" style="color: #127DB3; font-family: sans-serif; font-size: 17px; font-weight: 400; line-height: 160%;">support@test.com</a>
+</td>
+</tr>
+
+<!-- End of WRAPPER -->
+</table>
+
+<!-- WRAPPER -->
+<!-- Set wrapper width (twice) -->
+<table border="0" cellpadding="0" cellspacing="0" align="center"
+width="560" style="border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit; max-width: 560px;" class="wrapper">
+<tr>
+<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+padding-top: 25px;" class="social-icons"><table
+width="256" border="0" cellpadding="0" cellspacing="0" align="center" style="border-collapse: collapse; border-spacing: 0; padding: 0;">
+</table>
+</td>
+</tr>
+
+<!-- End of WRAPPER -->
+</table>
+
+<!-- End of SECTION / BACKGROUND -->
+</td></tr></table>
+
+</body>
+</html>
+`
 })
 
 module.exports = {verifyTemplate}
